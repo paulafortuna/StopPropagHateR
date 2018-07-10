24da6736ab185c6b722bf9f01f81e00f30d4cc09
paulafortuna <ei12025@fe.up.pt>
2018-07-10 13:21:54 +0200

    Package Stop PropagHate can not load files :(

---
+++ StopPropagHateR/R/stopPropagHate.R
@@ -0,0 +1,65 @@
+
+#' Stop PropagHate function
+#'
+#' This function allows you to evaluate if a set of text messages constains hate speech.
+#' @param data_frame, is a data.frame containing a column named "text".
+#' @param hate_type, is the type of hate you want to measure. It can be general "hate" speech, "sexism" or "racism".
+#' @param language, we provide this funtion both for Enlish "en" and Portuguese "pt".
+#' @keywords hate speech, racism, sexism
+#' @export
+#' @examples texts_data_frame <- data.frame(id = c(1,2,3), text = c("Lugar de mulher e na cozinha, isto e a verdade", "gorda e feia", "mais uma mensagem de teste"))
+#' stopPropagHate(texts_data_frame, "sexism", "pt")
+stopPropagHate <- function(data_frame, hate_type, language){
+
+  # control of inputs
+  try(
+    if(length(intersect(colnames(data_frame),"text")) == 0){
+      stop("No column named text in data_frame argument.")
+    } else {
+      try(
+        if(length(intersect(hate_type, c("hate", "sexism", "racism"))) == 0){
+          stop("The hate type is not available. Try with hate, sexism or racism.")
+        } else {
+          try(
+            if(length(intersect(language, c("en", "pt", "racism"))) == 0){
+              stop("The language is not available. Try with en (English) or pt (Portuguese).")
+            } else {
+
+              ## define tokenizer and model based on hate_type and language
+              model_filename <- paste(c("data/models/", hate_type, "_", language, ".h5"), collapse = '')
+
+              print("HELLO")
+              #tokenizer_filename <- paste(c("data/tokenizer_", language, ".h5"), collapse = '')
+              #devtools::install_github("rstudio/reticulate")
+              tokenizer_filename <- "tokenizer_pt"
+
+              ## apply model ##
+              # read model based on hate_type and language
+              reticulate::use_python("/Users/paulafortuna/anaconda3/envs/r-tensorflow/bin/python")
+              mod <- kerasR::keras_load(model_filename)
+
+              ## convert texts_vector to word_embeddings ##
+              #read tokenizer
+              tokenizer <- keras::load_text_tokenizer(tokenizer_filename)
+
+              #apply tokenizer to the data
+              sequences <- keras::texts_to_sequences(tokenizer, data_frame$text)
+              x_test <- keras::pad_sequences(sequences, maxlen = 100)
+
+
+
+              # apply model to the data
+              classified_data <- kerasR::keras_predict_proba(mod, x_test)
+
+              # round data to 0 for no hate or to 1 for hate
+              classified_data <- round(classified_data)
+              # return result
+              return(classified_data)
+            }
+          )
+        }
+      )
+    }
+  )
+}
+
